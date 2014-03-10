4596cd2f8bff82cae314334c1a000243fa80370f
Jeffrey Hearn <cowbellemoo@gmail.com>
2014-03-10 03:33:11 -0400

    omg work

--- public/assets/js/history.js
+++ public/assets/js/history.js
@@ -4,8 +4,19 @@ $(function() {
 	// Timeline query
 	var timelineSlug = $("body").data("timeline");
 
-	// Timeline files
-	var timelineDataFiles = [ "india" ];
+	// Timeline meta
+	var timelineMeta = {
+		"index": {
+			"title": "Tea Through Time",
+			"description": "<p>Drag to move the timeline. Events are grouped by type: Political, Cultural, and Economic</p>",
+			"files": [ "india" ]
+		},
+		"india": {
+			"title": "Timeline of Events in India",
+			"description": "<p>Drag to move the timeline. Events are grouped by type: Political, Cultural, and Economic</p>",
+			"files": [ "india" ]
+		}
+	}
 
 	/* TIMELINE CONFIGS
 	-------------------------------------------------- */
@@ -15,15 +26,15 @@ $(function() {
 		source:             {},
 		embed_id:           'timeline-embed',				//OPTIONAL USE A DIFFERENT DIV ID FOR EMBED
 		start_zoom_adjust:  '3',							//OPTIONAL TWEAK THE DEFAULT ZOOM LEVEL
-		hash_bookmark:      true,							//OPTIONAL LOCATION BAR HASHES
+		hash_bookmark:      false,							//OPTIONAL LOCATION BAR HASHES
 		lang:               'en',							//OPTIONAL LANGUAGE
 	}
 
     var timelineObj = {
 		"timeline": {
-			"headline":"The Main Timeline Headline Goes here",
+			"headline": timelineMeta[timelineSlug].title,
 			"type":"default",
-			"text":"<p>Intro body text goes here, some HTML is ok</p>",
+			"text": timelineMeta[timelineSlug].description,
 			"asset": {},
 			"date": [],
 			"era": []
@@ -31,7 +42,7 @@ $(function() {
 	};
 
 	// Combine timeline data files into a single list
-	$.each( timelineDataFiles, function( i, file ) {
+	$.each( timelineMeta[timelineSlug].files, function( i, file ) {
 		$.getJSON( siteGlobals.basedir + "data/timeline-"+file+".json", function( data ) {
 			$.each( data.date, function( i, date ) {
 				timelineObj.timeline.date.push( date );
@@ -46,6 +57,9 @@ $(function() {
 
 			// Render the timeline
 			createStoryJS( timelineConfigMd );
+
+			// Render tweaks
+			//$("#timeline-embed .era div").addClass();
 		});
 	});
 
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,20 +13,29 @@ $(function() {
 	/* SECONDARY MENU
 	-------------------------------------------------- */
 	// Create Dynamic Menu
-	$('h2').each( function(index, element){
+	$('h2, h3').each( function(index, element){
 		// Add ID to each header
 		var headerText = $(element).html();
 		headerText = headerText.toLowerCase();
+		headerText = headerText.replace( /'/ig, "" );
 		headerText = headerText.replace( / /ig, "-" );
 		$(element).attr( 'id', headerText );
 
 		// Add menu item
-		$(".page-nav > .nav").append('<li><a href="#' + headerText + '">' + $(element).html() + '</a></li>')
+		if ( $(this).is('h2') ) {
+			$(".page-nav-container > .nav").append('<li><a href="#' + headerText + '">' + $(element).html() + '</a><ul class="nav"></ul></li>');
+		} else {
+			$(".page-nav-container > .nav > li").last().children('.nav').append('<li><a href="#' + headerText + '">' + $(element).html() + '</a></li>');
+		}
 	});
+	// Clean up empty ULs
+	$(".page-nav-container .nav:empty").remove();
 
-	/*
+
 	// Initialize the scrollspy and refresh it after content is set
-	$('body').scrollspy({ target: '.page-nav' });
+	$('body').scrollspy({ target: '.page-nav-container' });
+	
+	/*
 	$(window).on('load', function () {
 		$('body').scrollspy('refresh');
 	});
@@ -37,5 +46,26 @@ $(function() {
 		e.preventDefault();
 		$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 400);
 	});
+
+
+	var $sideBar = $('.page-nav-container');
+	//console.log($sideBar.offset().top);
+
+	setTimeout(function () {
+		$sideBar.affix({
+			offset: {
+				top: function () {
+					var offsetTop = $sideBar.offset().top;
+					//var marginTop = $sideBar.css('marginTop');
+					//console.log(marginTop);
+					//var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+					//var navOuterHeight = $('.page-nav').height();
+
+					//return (this.top = offsetTop - navOuterHeight - sideBarMargin);
+					return (this.top = offsetTop + 80);
+				}
+			}
+		})
+	}, 200);
 
 });
