60df0bc1b6a535e181e61f9479b83c76347c900c
Jeffrey Hearn <cowbellemoo@gmail.com>
2014-04-01 20:44:52 -0400

    Citation system

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,63 @@
 
 $(function() {
 
+
+
+	/* SEXY BACKGROUNDS SO SEXY
+	-------------------------------------------------- */
+	$.stellar();
+
+
+	/* ENDNOTES
+	-------------------------------------------------- */
+	
+	// Get reference data
+	$.getJSON( siteGlobals.path + "data/references.json", function( refs ) {
+		// housekeeping
+		var noteIndex = 0,
+		    refIDs = [];
+		// Check for inline citations
+		if ( $('sup[data-ref-id!=""]') ){
+			// Add a header to the section
+			var endnotes = $('#references').before('<h2 class="ref-header">References</h2>');
+			// Iterate over inline citations
+			$('sup[data-ref-id]').each(function(i){
+				var refID = $(this).data('ref-id');
+				// Check if the reference item has been referenced yet
+				if ( $.inArray( refID, refIDs ) == -1 ){
+					// Add the reference to the list, increment the notes
+					refIDs.push(refID);
+					noteIndex++;
+					// Create a reference item with link back to citation
+					endnotes.append( '<li class="ref-item small" id="ref-item-'+noteIndex+'">' + 
+						'<a href="#ref-citation-'+noteIndex+'" class="ref-item-reciprocal-link"><span class="glyphicon glyphicon-arrow-up"></span></a> ' + 
+						refs[ refID ].title + '</li>' );
+					// Add a link to the reference, text for the citation, and tooltip text.
+					// (since this is an async block, must init tooltips explicitly rather 
+					// than use the catch-all since it will have already completed)
+					$(this)
+						.attr('data-toggle', 'tooltip')
+						.attr('id', 'ref-citation-'+noteIndex)
+						.attr('data-placement', 'top')
+						.attr('title', refs[ refID ].title)
+						.append('<a href="#ref-item-'+noteIndex+'">'+noteIndex+'</a>')
+						.tooltip();
+				} else {
+					// Insert the two reciprocal links? Sublist of citations?
+				}
+			});
+		}
+
+	});
+
+	// Init any other tooltips
+	$('*[data-toggle="tooltip"]').tooltip();
+
+
+
+
+
+
 	/* SECONDARY MENU
 	-------------------------------------------------- */
 	// Create Dynamic Menu
@@ -48,15 +105,5 @@ $(function() {
 		});
 		$sideBar.width( $sideBar.width() );
 	}, 200);
-
-
-	/* SEXY BACKGROUNDS SO SEXY
-	-------------------------------------------------- */
-	$.stellar();
-
-
-	/* ENDNOTES
-	-------------------------------------------------- */
-	$('*[data-toggle="tooltip"]').tooltip();
 
 });
